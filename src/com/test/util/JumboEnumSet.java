386ef104fad34065cf84fed910a34a521729e63c
jiangyongbing24 <253684597@qq.com>
2019-08-13 17:36:34 +0800

    ...

--- src/com/test/util/JumboEnumSet.java
+++ src/com/test/util/JumboEnumSet.java
@@ -1,36 +1,119 @@
 package com.test.util;
 
-import java.util.Iterator;
+import com.test.lang.IllegalStateException;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * EnumSet的私有实现类，用于特大型枚举类型（即大于64）
+ * */
 class JumboEnumSet<E extends Enum<E>> extends EnumSet<E> {
     private static final long serialVersionUID = 334349849919042784L;
 
+    /**
+     * 该Set的位向量表示，使用long数组表示位向量
+     * 当一个不够用的时候，使用多个long类型的作为位向量
+     * */
+    private long elements[];
+
+    private int size = 0;
+
     JumboEnumSet(Class<E> elementType, Enum<?>[] universe) {
         super(elementType, universe);
+        //初始化一个数组，这个数组的所有bit数大于枚举类型数量
+        //使用(universe.length+63)/64只取整数位(等于universe.length >>> 64)即可得到一个正好可以容纳的尺寸
+        //只加63而不是64是避免universe.length正好是64的倍数，而数组长度多初始化一个从而浪费资源
+        elements = new long[(universe.length + 63) >>> 6];
     }
 
-    @Override
-    void addAll() {
-
-    }
-
-    @Override
     void addRange(E from, E to) {
+        int fromIndex = from.ordinal() >>> 6;
+        int toIndex = to.ordinal() >>> 6;
 
+        //如果两值相等，表示指向他们位置的向量是数组中的同一个元素
+        if(fromIndex == toIndex){
+            //定位到这个向量，然后初始化向量
+            elements[fromIndex] = (-1L >>> (from.ordinal() - to.ordinal() - 1) << from.ordinal());
+        }else{
+            elements[fromIndex] = (-1L << from.ordinal());//初始化elements[fromIndex]
+            for(int i = fromIndex;i < fromIndex - toIndex;i++)
+                elements[i] = -1L;
+            elements[toIndex] = (-1L >>> -to.ordinal()-1);//初始化elements[toIndex]
+        }
+        size = to.ordinal() - fromIndex + 1;
     }
 
-    @Override
+    void addAll() {
+        for(int i = 0;i < universe.length - 1;i++)
+            elements[i] = -1;
+        elements[universe.length-1] = -1L >>> -universe.length;
+        size = universe.length;
+    }
+
     void complement() {
-
+        for (int i = 0; i < universe.length; i++)
+            elements[i] = ~elements[i];
+        elements[universe.length-1] &= -1L >>> -universe.length;
+        size = universe.length - size;
     }
 
-    @Override
     public Iterator<E> iterator() {
-        return null;
+        return new EnumSetIterator();
     }
 
-    @Override
+
+    /** 一个用于JumboEnumSet的迭代器 */
+    private class EnumSetIterator<E extends Enum<E>> implements Iterator<E>{
+        //记录当前elements元素的尚未返回的位向量
+        long unseen;
+
+        //尚未迭代到的元素的索引
+        int unseenIndex = 0;
+
+        //表示此迭代器返回的最后一个元素的位
+        long lastReturned = 0;
+
+        //lastReturned所在elements的索引
+        int lastReturnedIndex = 0;
+
+        EnumSetIterator(){unseen=elements[0];}
+
+        public boolean hasNext() {
+            //当unseen等于0，并且unseenIndex没有索引到最后一个元素的时候，继续进入循环
+            while(unseen == 0 && unseenIndex < elements.length - 1)
+                unseen = elements[++unseenIndex];
+            return unseen != 0;
+        }
+
+        public E next() {
+            //如果已经没有元素了，抛出异常
+            if(!hasNext())
+                throw new NoSuchElementException();
+            //获取接下来需要返回的位向量
+            lastReturned = unseen & -unseen;
+            //记录最后一次返回的索引
+            lastReturnedIndex = unseenIndex;
+            //去掉已经被迭代的元素
+            unseen -= lastReturned;
+            return (E)universe[(lastReturnedIndex << 6) + Long.numberOfTrailingZeros(lastReturned)];
+        }
+
+        public void remove(){
+            if(lastReturned == 0)
+                throw new IllegalStateException();
+            //记录原来没有修改的元素
+            final long oldElements = elements[lastReturnedIndex];
+            //修改位向量对应位为0
+            elements[lastReturnedIndex] &= ~lastReturned;
+            //如果修改成功，大小减一
+            if(oldElements != elements[lastReturnedIndex])
+                size--;
+            lastReturned = 0;
+        }
+    }
+
     public int size() {
-        return 0;
+        return size;
     }
 }
--- src/com/test/util/RegularEnumSet.java
+++ src/com/test/util/RegularEnumSet.java
@@ -8,11 +8,27 @@ import java.util.NoSuchElementException;
 
 /**
  * EnumSet的私有实现类，用于"常规大小"的枚举型（即小于等于64）
+ *
+ * 对于这个类的位运算符做一个介绍：
+ * 对于Java的>>>,>>,<<位运算符号，如果这些运算符的左边为int，则运算符的右边只有低5位有效，如果运算符的左边为long，则运算符的右边只有低6位有效
+ * 举例：int a = 123456789; 则 a >> 0b10101 == a >> 0b110101 为true，因为只有低5位10101会被算作有效的。
+ * 换个说法就是：位运算符会以 运算符的左边的数据类型在内存中存储的bit数 作为一个运算周期
+ * 比如说int在内存中是使用32bit存储的，那么运算符以32作为周期，上述的 a >> 0b10101 == a >> 0b110101 转换位十进制为 a >> 21 == a >> 53，其中21和53差了一个32周期
+ * 通过上述的描述，我们能理解的是，当运算符右边为一个小于0的数的时候，它可以转换为对应的整数然后进行运算，这个整数减去负数之差除以32的余数要为0
  * */
 class RegularEnumSet<E extends Enum<E>> extends EnumSet<E> {
     private static final long serialVersionUID = 3411599620347842686L;
 
-    //该集的位向量表示
+    /**
+     * 该集的位向量表示
+     *
+     * 比如现有一个枚举:enum MyEnum{one,two,three;}
+     * RegularEnumSet存储的就是此枚举值，那么位向量为0x7，转换为二进制为00...0111
+     * 从二进制的最右边开始，1bit代表对应的枚举的位置，
+     * 即如果RegularEnumSet含有MyEnum.two，则elements二进制的 MyEnum.two.origin() 位一定为1，其他的枚举值类似
+     * 当进行了 addRange(MyEnum.one,MyEnum.two)，elements变为00...0110，再进行 complement(),
+     * elements变为00...0001
+     * */
     private long elements = 0L;
 
     RegularEnumSet(Class<E> elementType, Enum<?>[] universe) {
@@ -25,7 +41,8 @@ class RegularEnumSet<E extends Enum<E>> extends EnumSet<E> {
     void addRange(E from, E to) {
         //from.ordinal()-to.ordinal()-1得到区间[from,to]的元素数量的相反数
         //-1L不带符号右移这个相反数会得到二进制有对应数量1bit的数
-        //再对这个数进行左位移
+        //再对这个数进行from.ordinal()的左位移，向量最左边的1bit的2的幂值等于from.ordinal()
+        //只有实例化RegularEnumSet的时候，universe包含全部的枚举，elements向量才能正确的指向
         elements = (-1L >>> (from.ordinal() - to.ordinal() - 1) << from.ordinal());
     }
 
@@ -34,18 +51,26 @@ class RegularEnumSet<E extends Enum<E>> extends EnumSet<E> {
      * 在没有进行这个动作之前，RegularEnumSet是个空set
      * */
     void addAll() {
-        // 根据universe的长度，得到一个值
-        // 这个值的二进制的1bit的数量 = universe.length
+        // 根据universe的长度，得到一个符合下述条件的值:
+        // 这个值的二进制的1bit的数量 == universe.length
+        //
         // 通过对-1L >>> (64-universe.length)，即可得到符合上述条件的值
-        // 而上述公式又等于 -1L >>> -universe.length
+        // 而上述公式又等于 -1L >>> -universe.length，为什么等于查看12行的java位运算符号的介绍
         if (universe.length != 0)
             elements = -1L >>> -universe.length;
     }
 
-    /** 转为EnumSet的补码 */
+    /**
+     * 转为EnumSet的补码
+     * 在进行这一步之前，确定进行了addAll或者addRange操作
+     * */
     void complement() {
         if(universe.length != 0){
+            //取elements的补码
             elements = ~elements;
+            //用这个补码和 初始的位向量(-1L >>> -universe.length) 进行与运算
+            //因为 初始的位向量 对应枚举的位置全为1，与现有的位向量的补码进行与，
+            //得到一个原来位向量的一个补码位向量
             elements &= -1L >>> -universe.length;
         }
     }
@@ -58,8 +83,10 @@ class RegularEnumSet<E extends Enum<E>> extends EnumSet<E> {
      * 一个用来迭代 EnumSet 的迭代器
      * */
     private class EnumSetIterator<E extends Enum<E>> implements Iterator<E>{
+        //还没迭代的元素的位向量
         long unseen;
 
+        //表示迭代器最后一个元素的位
         long lastReturned = 0;
 
         EnumSetIterator(){unseen = elements;}
@@ -72,15 +99,20 @@ class RegularEnumSet<E extends Enum<E>> extends EnumSet<E> {
         public E next() {
             if(unseen == 0)
                 throw new NoSuchElementException();
+            //lastReturned指向最低位的1bit
             lastReturned = unseen & -unseen;
+            //unseen去除最后一个元素的位向量
             unseen -= lastReturned;
+            //返回对应位置的元素
             return (E) universe[Long.numberOfTrailingZeros(lastReturned)];
         }
 
         public void remove(){
             if(lastReturned == 0)
                 throw new IllegalStateException();
+            //移除对应位置上的位向量
             elements &= ~lastReturned;
+            //lastReturned重置
             lastReturned = 0;
         }
     }
@@ -94,21 +126,119 @@ class RegularEnumSet<E extends Enum<E>> extends EnumSet<E> {
         if(e == null)
             return false;
         Class<?> eClass = e.getClass();
+        //如果类型对应不上，直接返回false
         if(eClass != elementType || eClass.getSuperclass() != elementType)
             return false;
 
+        //使用1左移e.ordinal()位，这个数字的二进制除了e.ordinal()位为1bit，其余均为0bit
+        //使用elements进行与运算之后，如果elements的第e.ordinal()位为1，则结果不会为0，表示包含元素e
         return (elements & (1L << ((Enum<?>)e).ordinal())) != 0;
     }
 
+    public boolean add(E e){
+        //如果e不是枚举类型，抛出异常
+        typeCheck(e);
+
+        long oldElements = elements;
+        //对应位置的位改为1
+        elements |= 1L << ((Enum<?>)e).ordinal();
+        return elements != oldElements;
+    }
+
+    public boolean remove(Object e){
+        if(e == null)
+            return false;
+        Class<?> eClass = e.getClass();
+        //如果类型对应不上，直接返回false
+        if(eClass != elementType || eClass.getSuperclass() != elementType)
+            return false;
+
+        long oldElements = elements;
+        //对应位置的位向量改为0
+        elements &= ~(1L << ((Enum<?>)e).ordinal());
+        return elements != oldElements;
+    }
+
+    public boolean containsAll(Collection<?> c){
+        if(!(c instanceof RegularEnumSet))
+            return super.containsAll(c);
+
+        RegularEnumSet<?> es = (RegularEnumSet<?>) c;
+        if(es.elementType != elementType)
+            return es.isEmpty();//如果类型不相等，但是为空，返回true
+
+        //如果位向量相等，返回true
+        return (es.elements & ~elements) == 0;
+    }
+
+    public boolean addAll(Collection<? extends E> c){
+        if(!(c instanceof RegularEnumSet))
+            return super.addAll(c);
+
+        RegularEnumSet<?> es = (RegularEnumSet<?>) c;
+        if(es.elementType != elementType){
+            if(isEmpty())//如果类型不相等，但是为空，返回true
+                return true;
+            else
+                throw new ClassCastException(es.elementType + " != " + elementType);
+        }
+
+        long oldElements = elements;
+        elements |= es.elements;
+        return elements != oldElements;
+    }
+
+    public boolean removeAll(Collection<?> c){
+        if(!(c instanceof RegularEnumSet))
+            return super.removeAll(c);
+
+        RegularEnumSet<?> es = (RegularEnumSet<?>) c;
+        if(es.elementType != elementType)
+            return false;
+
+        long oldElements = elements;
+        elements &= ~es.elements;
+        return elements != oldElements;
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        if (!(c instanceof RegularEnumSet))
+            return super.retainAll(c);
+
+        RegularEnumSet<?> es = (RegularEnumSet<?>)c;
+        if (es.elementType != elementType) {
+            boolean changed = (elements != 0);
+            elements = 0;
+            return changed;
+        }
+
+        long oldElements = elements;
+        elements &= es.elements;
+        return elements != oldElements;
+    }
+
+    public void clear() {
+        elements = 0;
+    }
+
+    public boolean equals(Object o) {
+        if (!(o instanceof RegularEnumSet))
+            return super.equals(o);
+
+        RegularEnumSet<?> es = (RegularEnumSet<?>)o;
+        if (es.elementType != elementType)
+            return elements == 0 && es.elements == 0;
+        return es.elements == elements;
+    }
+
     public static void main(String[] args) {
-        Enum<MyEnum>[] myEnum = new Enum[]{MyEnum.one,MyEnum.two};
+        Enum<MyEnum>[] myEnum = new Enum[]{MyEnum.one,MyEnum.two,MyEnum.three};
         RegularEnumSet<MyEnum> set = new RegularEnumSet<MyEnum>(MyEnum.class,myEnum);
         System.out.println(Long.toBinaryString(set.elements));
         set.addAll();
-//        set.addRange();
-//        System.out.println(Long.toBinaryString(set.elements));
+        set.addRange(MyEnum.two,MyEnum.one);
+        System.out.println(Long.toBinaryString(set.elements));
         set.complement();
         System.out.println(Long.toBinaryString(set.elements));
-//        System.out.println(set.size());
     }
 }
